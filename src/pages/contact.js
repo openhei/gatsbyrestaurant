a05df284057f01f5378697d1d27de4f8e1d8345b
Chris Yi <chris@Chris.local>
2020-01-13 10:27:39 -0500

    second commit

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title="Contact" keywords={[`gatsby`, `application`, `react`]} />
+    <h3>this is about Contact</h3>
+  </Layout>
+)
+
+export default ContactPage
---
+++ src/pages/menu.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const MenuPage = () => (
+  <Layout>
+    <SEO title="Menu" keywords={[`gatsby`, `application`, `react`]} />
+    <h3>this is about Menu</h3>
+  </Layout>
+)
+
+export default MenuPage
